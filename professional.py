d5c104875f1f74f5c828241b5069ea280ed2bc30
Arjun Banga <banga>
2023-11-12 15:50:17 -0400

    professional frotnend

---
+++ professional.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import os
+
+# Assuming the following directory structure:
+# /path/to/recordings/folder/ -> contains audio files
+# /path/to/outputs/folder/ -> contains text summaries with the same filenames
+
+recordings_folder_path = './sample_audio'
+outputs_folder_path = './sample_audio'
+
+print(os.listdir('./sample_audio'))
+# List all the patient record files (assuming the extension for audio files is .mp3)
+patient_records = [f for f in os.listdir(recordings_folder_path) if f.endswith('.aac')]
+
+print(patient_records)
+
+# Create a select box for the user to choose a patient record
+selected_record = st.selectbox('Select a patient record', patient_records)
+
+# Display the selected patient's name (assuming the filename structure is 'FirstName_LastName.mp3')
+patient_name = selected_record[:-4].replace('_', ' ')  # This removes the file extension and replaces underscores with spaces
+st.text('Name: ' + patient_name)
+
+# Display the audio player for the selected patient's recording
+audio_file_path = os.path.join(recordings_folder_path, selected_record)
+audio_file = open(audio_file_path, 'rb')
+st.audio(audio_file.read(), format='audio/aac')
+
+# Display the summary for the selected patient's record
+# Assuming the text summaries have the same filename but with .txt extension
+summary_file_path = os.path.join(outputs_folder_path, selected_record.replace('.aac', '.txt'))
+if os.path.exists(summary_file_path):
+    with open(summary_file_path, 'r') as file:
+        summary_text = file.read()
+    st.text('Summary:')
+    st.write(summary_text)
+else:
+    st.error('Summary file does not exist for the selected record.')
+
